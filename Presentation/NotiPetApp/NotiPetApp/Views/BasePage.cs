22b4d432f2c9860e71eb8cbf9678f8ba5ecc97e2
rafae <rafaelfernandez0512@gmail.com>
2022-02-01 19:18:42 -0400

    create page and folders

---
+++ Presentation/NotiPetApp/NotiPetApp/Views/BasePage.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace NotiPetApp.Views
+{
+    public class BasePage:ContentPage
+    {
+        
+    }
+}
---
+++ Presentation/NotiPetApp/NotiPetApp/Views/TabMenuPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotiPetApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabMenuPage : ContentPage
+    {
+        public TabMenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
